d221e479fcc14184ee51f6ca82740f60b52dad2d
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-07-28 14:40:39 +0100

    gulp files concatenated through concate plugin

--- lynda-gulp-app/gulpfile.js
+++ lynda-gulp-app/gulpfile.js
@@ -1,8 +1,35 @@
 //use nodejs's require command to bring in gulp library & assign it to a variable called gulp
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
+
+var coffeeSources = ['components/coffee/tagline.coffee'];
+
+var jsSources = [
+    'components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js'
+];
 
 //use gulps task method to create tasks
-gulp.task('log', function() {
-  gutil.log('Workflows are awesome');
+gulp.task('coffee', function() {
+    //define source file
+    gulp.src(coffeeSources)
+        //convert cofeescript to javascript
+        .pipe(coffee({ bare: true })
+        //display log message incase of errors
+        .on('error', gutil.log))
+        //out put to destination file
+        .pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function() {
+    //gather input sources to be concatenated
+    gulp.src(jsSources)
+        //concatenate js file into a script.js file
+        .pipe(concat('script.js'))
+        //output final file to destination folder
+        .pipe(gulp.dest('builds/development/js'))
 });
