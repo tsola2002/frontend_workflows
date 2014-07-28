50f0c44bd4c14e4ada9460b153ea04d8027be3c9
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-07-28 16:44:10 +0100

    static html watcher changes added

--- lynda-gulp-app/gulpfile.js
+++ lynda-gulp-app/gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
+    connect = require('gulp-connect'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -17,7 +18,9 @@ var jsSources = [
 
 var sassSources = ['components/sass/style.scss'];
 
+var htmlSources = ['builds/development/*.html'];
 
+var jsonSources = ['builds/development/js/*.json'];
 
 //use gulps task method to create tasks
 gulp.task('coffee', function() {
@@ -40,6 +43,8 @@ gulp.task('js', function() {
         .pipe(browserify())
         //output final file to destination folder
         .pipe(gulp.dest('builds/development/js'))
+        //do a reload on the server
+        .pipe(connect.reload())
 });
 
 gulp.task('compass', function() {
@@ -55,6 +60,8 @@ gulp.task('compass', function() {
             //spit log message if there are any errors
             .on('error', gutil.log))
         .pipe(gulp.dest('builds/development/css'))
+        //do a reload on the server
+        .pipe(connect.reload())
 });
 
 
@@ -64,7 +71,36 @@ gulp.task('watch', function() {
     gulp.watch(jsSources, ['js']);
     //when any file with a .scss extension changes, we run the compass task
     gulp.watch('components/sass/*.scss', ['compass']);
+    //when any html file changes do a livereload
+    gulp.watch(htmlSources, ['html']);
+    //when any json file changes do a livereload
+    gulp.watch(jsonSources, ['json']);
+
+});
+
+gulp.task('connect', function() {
+    //use connect variable's of the server method to create a server
+    connect.server({
+        //specify the root of your application
+        root: 'builds/development/',
+        //turn on livereload feature
+        livereload: true
+    });
+});
+
+gulp.task('html', function() {
+    //set input sources to html files
+    gulp.src(htmlSources)
+    //pipe the sources to livereload
+        .pipe(connect.reload())
+});
+
+gulp.task('json', function() {
+    //set input sources to json files
+    gulp.src(jsonSources)
+    //pipe the sources to livereload
+        .pipe(connect.reload())
 });
 
 //custom gulp task to run all tasks
-gulp.task('default', ['coffee', 'js', 'compass', 'watch'])
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch'])
